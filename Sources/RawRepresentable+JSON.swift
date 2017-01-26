c2b9c6ad5e87a5cd076e9dce66c9cdc7cdf99c99
Pavel Procházka <pprochazka72@gmail.com>
2017-01-26 22:03:08 +0100

    Add JSON typealias

---
+++ Sources/RawRepresentable+JSON.swift
@@ -0,0 +1,15 @@
+//
+//  RawRepresentable+JSON.swift
+//  TODO-Foundation
+//
+//  Created by Pavel Procházka on 26/01/2017.
+//
+//
+
+import Foundation
+
+public typealias JSON = [String: Any]
+
+extension RawRepresentable {
+	typealias RawValue = JSON
+}
